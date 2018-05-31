7ba7dc13c0ac8e6e79f43406993a9a8fa58de4cc
Molly Van Dop <molly.van.dop@gmail.com>
2018-05-31 09:23:39 -0700

    Added Molly's local file path to Box.R

--- box.R
+++ box.R
@@ -2,5 +2,5 @@
 
 box <- c("C:/Users/Matthew/Box Sync/CalDiversity_2018_Box",
                  "David's local box folder",
-                 "Molly's local box folder")
+                 "C:\Users\Molly\Box\CalDiversity_2018_Box")
 box <- box[dir.exists(box)]
